e3e76907290cafbd0aba70463fef296f6c004260
Stephen-Uche <osedummesteven@yahoo.com>
2025-08-13 12:02:20 +0200

    changes

--- CSharpApp/helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
+++ CSharpApp/helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93de0c631e57a667e5daa7b63552f4443bb9dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
