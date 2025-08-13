e776f2771459106e25d444351f6737b5dced1fba
Stephen-Uche <osedummesteven@yahoo.com>
2025-08-13 12:20:13 +0200

    cahnges

--- CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93de0c631e57a667e5daa7b63552f4443bb9dd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e76907290cafbd0aba70463fef296f6c004260")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
