de8ec181f22740e2551d183591fda6e676c122fa
60steose@gafe.molndal.se <60steose@gafe.molndal.se>
2025-08-27 15:24:04 +0200

    Created gitignore

--- CSharpApp/helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
+++ CSharpApp/helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8072edb6d548a88137ab858634ab495993eea46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c7884be75134ce7f217055efe60f5b975f1c579")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
