5b8b3bbbf4d42d7456e3664f288922e714547ab5
60steose@gafe.molndal.se <60steose@gafe.molndal.se>
2025-08-27 15:32:22 +0200

    changes

--- CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a343870caaa78e22c696d51a82166ef6a12dbf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ea4a94da2108fe75da34acba328db60e102410")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
