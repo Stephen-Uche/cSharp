21cb7d38706ae47423d74c5ac627c753509dcbb3
60steose@gafe.molndal.se <60steose@gafe.molndal.se>
2025-08-27 15:50:48 +0200

    Ignoregit

--- CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31cf267ceb8204971ee4d8d4f40df5bcef968666")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae480f049f3232b2a0133a235cb43cd5e9a83b79")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
