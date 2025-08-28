5c92fa10038cf6bc18c43a23f0a6fe78cd28e4e1
60steose@gafe.molndal.se <60steose@gafe.molndal.se>
2025-08-28 09:35:12 +0200

    Edit gitignore

--- CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ CSharpApp/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae480f049f3232b2a0133a235cb43cd5e9a83b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21cb7d38706ae47423d74c5ac627c753509dcbb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
